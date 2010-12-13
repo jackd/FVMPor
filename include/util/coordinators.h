e1fbbd519da7ae19c8f29f78675442cefe18d939
Ben Cumming <cummingb@cummingb.(none)>
2010-12-13 17:30:55 +1000

    improved CoordTraits and small correction to give more accurate times for F evaluations in preconditioner

---
+++ include/util/coordinators.h
@@ -0,0 +1,25 @@
+#ifndef COORDINATORS_H
+#define COORDINATORS_H
+
+//#include <lin/impl/rebind.h>
+#include <lin/coordinators/gpu/coordinator.h>
+#include <lin/lin.h>
+
+namespace util{
+
+template <typename T>
+struct CoordTraits{
+    static bool is_device() {return false;};
+};
+template <>
+struct CoordTraits<lin::gpu::Coordinator<int> >{
+    static bool is_device() {return true;};
+};
+template <>
+struct CoordTraits<lin::gpu::Coordinator<double> >{
+    static bool is_device() {return true;};
+};
+
+} // namespace util
+
+#endif

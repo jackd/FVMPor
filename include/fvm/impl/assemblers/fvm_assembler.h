17aed8b399d00225eb9707b76a98fa530962804d
Ben <ben@benLaptop.(none)>
2011-03-13 07:44:35 +1000

    removed solver and fvm assembler classes that used iterators, and promoted versions that use minlin and that use "compact" form that requires that the physics provides the residual

--- include/fvm/impl/assemblers/fvm_assembler.h
+++ include/fvm/impl/assemblers/fvm_assembler.h
@@ -1,9 +1,16 @@
+/**************************************************************************
+ * The compact assembler hands construction of the residual to the physics.
+ * This allows the implementer of the physics to implement a more efficient
+ * assembler than the default assembler, which can be orders of magnitude
+ * faster for meshes with many control volume faces
+ **************************************************************************/
 #ifndef FVM_ASSEMBLER_H
 #define FVM_ASSEMBLER_H
 
 #include <fvm/fvm.h>
 #include <fvm/mesh.h>
 #include <mpi/mpicomm.h>
+#include <util/timer.h>
 
 #include <vector>
 
@@ -16,19 +23,14 @@ public:
 
     typedef typename Physics::value_type value_type;
     typedef typename Physics::TVecDevice TVecDevice;
-    //typedef typename Iterator<value_type>::type iterator;
-    //typedef typename ConstIterator<value_type>::type const_iterator;
 
     FVMAssembler(const Mesh& m, Physics& p);
     const Mesh& mesh() const;
     Physics& physics();
-    //DEVICE
-    //template<typename Iterator>
-    //int compute_residual(
-    //    double time, const_iterator u, const_iterator up, Iterator res);
-    int compute_residual(
-        //double time, const TVecDevice &u, const TVecDevice &up, TVecDevice &res);
-        double time, TVecDevice &u, TVecDevice &up, TVecDevice &res);
+    int compute_residual( double time,
+                          const TVecDevice &u,
+                          const TVecDevice &up,
+                          TVecDevice &res       );
 private:
     FVMAssembler(const FVMAssembler&);
     FVMAssembler& operator=(const FVMAssembler&);
@@ -37,21 +39,14 @@ private:
     Physics& p;
     mpi::MPICommPtr mpicomm_;
 
-    std::vector<value_type> is_dirichlet;
 
     static const int variables_per_node = VariableTraits<value_type>::number;
-
-    static void add_in_place(value_type&, value_type);
-    static void subtract_in_place(value_type&, value_type);
-    static void add_in_place(value_type&, value_type, value_type);
-    static void subtract_in_place(value_type&, value_type, value_type);
-    static void divide_in_place(value_type&, double);
 };
 
 template<class Physics>
 FVMAssembler<Physics>::FVMAssembler(const Mesh& m, Physics& p)
-    : m(m), p(p), is_dirichlet(m.local_nodes()) {
-    mpicomm_ = m.mpicomm();
+    : m(m), p(p) {
+    mpicomm_ = m.mpicomm();    
 }
 
 template<class Physics>
@@ -65,137 +60,26 @@ Physics& FVMAssembler<Physics>::physics() {
 }
 
 template<class Physics>
-template<typename Iterator>
 int FVMAssembler<Physics>::compute_residual(
-    // DEVICE
-    //double time, const_iterator u, const_iterator up, Iterator res) {
-    //double time, const TVecDevice &u, const TVecDevice &up, TVecDevice &res) {
-    double time, TVecDevice &u, TVecDevice &up, TVecDevice &res) {
+    double time, const TVecDevice &u, const TVecDevice &up, TVecDevice &res) {
+    //std::cerr << "==========================" << std::endl;
+    //std::cerr << "Assembler residual compute" << std::endl;
+    //std::cerr << "==========================" << std::endl;
 
     // Preprocess
     physics().preprocess_evaluation(time, mesh(), u, up);
 
-    // DEVICE
-    // this method has been deprciated in the optimised version of the library
+    // find the residual
+    physics().residual_evaluation(time, mesh(), u, up, res);
 
-    /*
-    // Zero the right hand side
-    for (int i = 0; i < mesh().local_nodes(); ++i)
-        res[i] = value_type();
-
-    // Determine Dirichlet nodes
-    for (int i = 0; i < mesh().local_nodes(); ++i) {
-        // Given that some variables can be strictly algebraic everywhere on the domain
-        // we have to check every node, not just those that lie on the boundary
-        is_dirichlet[i] = physics().dirichlet(time, mesh().node(i));
-    }
-
-    {// Fluxes
-        int i = 0;
-
-        // Interior Flux
-        for (; i < mesh().interior_cvfaces(); ++i) {
-            const mesh::CVFace& cvf = mesh().cvface(i);
-            value_type flux = physics().flux(time, cvf, u);
-
-            int front_id = cvf.front().id();
-            if (front_id < mesh().local_nodes())
-                add_in_place(res[front_id], is_dirichlet[front_id], flux);
-            int back_id = cvf.back().id();
-
-            if (back_id < mesh().local_nodes())
-                subtract_in_place(res[back_id], is_dirichlet[back_id], flux);
-        }
-
-        // Boundary flux
-        for (; i < mesh().cvfaces(); ++i) {
-            const mesh::CVFace& cvf = mesh().cvface(i);
-            int back_id = cvf.back().id();
-            if (back_id < mesh().local_nodes()) {
-                value_type flux = physics().boundary_flux(time, cvf, u);
-                subtract_in_place(res[back_id], is_dirichlet[back_id], flux);
-            }
-        }
-
-    }
-
-    // Volume averaged terms
-    for (int i = 0; i < mesh().local_nodes(); ++i) {
-
-        const mesh::Volume& v = mesh().volume(i);
-
-        // Scale by volume
-        divide_in_place(res[i], v.vol());
-
-        // Source
-        value_type source = physics().source(time, v, u);
-        add_in_place(res[i], source);
-
-        // Left hand side
-        value_type left = physics().lhs(time, v, u, up);
-        subtract_in_place(res[i], left);
-    }
-    */
+    //for(int i=0; i<mesh().local_nodes(); i++)
+    //    std::cerr << res[i] << ' ';
+    //std::cerr << std::endl;
+    
     // Postprocess
-    physics().postprocess_evaluation(time, mesh(), u, up);
+    //physics().postprocess_evaluation(time, mesh(), u, up);
 
     return 0;
-}
-
-// Basic arithmetic functions on value_type
-template<class Physics>
-void FVMAssembler<Physics>::add_in_place(
-value_type& destination, value_type source) {
-    double* dest = reinterpret_cast<double*>(&destination);
-    const double* src = reinterpret_cast<const double*>(&source);
-    for (int i = 0; i < variables_per_node; ++i) {
-        dest[i] += src[i];
-    }
-}
-
-template<class Physics>
-void FVMAssembler<Physics>::add_in_place(
-value_type& destination, value_type is_dirichlet, value_type source) {
-    double* dest = reinterpret_cast<double*>(&destination);
-    const double* is_dir = reinterpret_cast<const double*>(&is_dirichlet);
-    const double* src = reinterpret_cast<const double*>(&source);
-    for (int i = 0; i < variables_per_node; ++i) {
-        if (!is_dir[i]) {
-            dest[i] += src[i];
-        }
-    }
-}
-
-template<class Physics>
-void FVMAssembler<Physics>::subtract_in_place(
-value_type& destination, value_type source) {
-    double* dest = reinterpret_cast<double*>(&destination);
-    const double* src = reinterpret_cast<const double*>(&source);
-    for (int i = 0; i < variables_per_node; ++i) {
-        dest[i] -= src[i];
-    }
-}
-
-template<class Physics>
-void FVMAssembler<Physics>::subtract_in_place(
-value_type& destination, value_type is_dirichlet, value_type source) {
-    double* dest = reinterpret_cast<double*>(&destination);
-    const double* is_dir = reinterpret_cast<const double*>(&is_dirichlet);
-    const double* src = reinterpret_cast<const double*>(&source);
-    for (int i = 0; i < variables_per_node; ++i) {
-        if (!is_dir[i]) {
-            dest[i] -= src[i];
-        }
-    }
-}
-
-template<class Physics>
-void FVMAssembler<Physics>::divide_in_place(
-value_type& destination, double val) {
-    double* d = reinterpret_cast<double*>(&destination);
-    for (int i = 0; i < variables_per_node; ++i) {
-        d[i] /= val;
-    }
 }
 
 // Definition of static member
